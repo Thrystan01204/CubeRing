8c618aea30c98d32012fc60b1860e06a1b66f2dd
Thrystan01204 <47844397+Thrystan01204@users.noreply.github.com>
2023-02-04 18:09:45 +0400

    This is a boss

---
+++ Boss.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func initialize(start_position, player_position):
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+
+	var random_speed = randf_range(0.56, 0.85)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	move_and_slide()
