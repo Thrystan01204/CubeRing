bfa5e09dbbff9e9e714585be164ef254ce783c1d
Thrystan01204 <47844397+Thrystan01204@users.noreply.github.com>
2023-02-04 19:24:49 +0400

    Petite modif

--- Boss.gd
+++ Boss.gd
@@ -21,7 +21,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	move_and_slide()
