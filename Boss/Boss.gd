3b7e7a74c5532a177fade56e321a843fdcfb9a0e
Thrystan01204 <47844397+Thrystan01204@users.noreply.github.com>
2023-02-05 12:55:43 +0400

    Boss

--- Boss/Boss.gd
+++ Boss/Boss.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var anim = $AnimationPlayer
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -11,8 +13,7 @@ func initialize(start_position, player_position):
 	look_at_from_position(start_position, player_position, Vector3.UP)
 	rotate_y(randf_range(-PI / 4, PI / 4))
 
-	var random_speed = randf_range(0.56, 0.85)
-	velocity = Vector3.FORWARD * random_speed
+	velocity = Vector3.FORWARD * SPEED
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
 func _physics_process(delta):
@@ -25,3 +26,15 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	move_and_slide()
+
+func _process(delta):
+	look_at(target.position, Vector3.UP)
+
+	if (target.position < 5):
+		anim.play("smash")
+	else:
+		anim.play("Idle_flying")
+
+func _on_smash_area_entered(area:Area3D):
+	if area.is_in_group("player"):
+		area.take_damage()
