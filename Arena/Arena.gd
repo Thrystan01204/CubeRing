23db421cbf2cfee77a374310efa74c5048e88025
Pierre Jaffuer <p.jaffuer@free.fr>
2023-02-17 09:00:43 +0400

    Move to Godot 4.0 RC2

--- Arena/Arena.gd
+++ Arena/Arena.gd
@@ -4,6 +4,7 @@ extends Node3D
 @onready var player : FPSPlayer = $FPSPlayer
 @onready var intro_cam : Camera3D = $Camera3D
 @onready var music_player : AudioStreamPlayer = $AudioStreamPlayer
+@onready var boss_anim : AnimationPlayer = $Boss_model/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,5 +17,4 @@ func _ready():
 	player.set_process(true)
 	player.set_process_input(true)
 	player.get_camera().make_current()
-
-	
+	boss_anim.play("Idle_flying") # TODO : Enable boss AI instead
